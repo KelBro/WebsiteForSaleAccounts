1f7c5084b414cab9fb9d4a1ee750301727f01761
Mr_Kel <papaez08@gmail.com>
2025-04-19 00:03:16 +1000

    completed frontend site

--- website/run.py
+++ website/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from config_reader import config
 from website.data import db_session
 from website.data.__all_models import catalog_accounts
@@ -20,9 +20,34 @@ def index():
         if 'session' in locals():
             session.close()
 
+
+@app.route('/account/info/<id_product>')
+def account_info(id_product):
+    session = db_session.create_session()
+    account = session.query(catalog_accounts.Catalog).filter_by(id=id_product).first()
+    session.close()
+
+    if not account:
+        abort(404)  # Если аккаунт не найден
+
+    return render_template('account_info.html', account=account)
+
+
+@app.route('/cart')
+def cart():
+    # Здесь должна быть логика получения товаров в корзине
+    return render_template('cart.html', cart_items=[], total_price=0)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
 
 def run_website():
     app.run(port=8080, host='127.0.0.1', debug=False, use_reloader=False)
